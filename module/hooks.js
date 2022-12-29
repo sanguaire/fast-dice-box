4efab655d1b53b4e24e2fed1519f84c5fced9af7
André Kurth <mail@andre-kurth.de>
2022-12-29 18:01:38 +0100

    - Optimize code organization

---
+++ module/hooks.js
@@ -0,0 +1,51 @@
+import {registerSettings} from "./settings.js";
+import {initializeToastr, newDiceRoll} from "./notification.js";
+
+let socket;
+
+export const registerHookHandlers = () =>{
+    Hooks.once('init',
+        async () => {
+            console.log('fast-dice-box | Initializing');
+
+            registerSettings();
+
+            // Preload Handlebars templates
+            await preloadTemplates();
+
+            // Register custom sheets (if any)
+            CONFIG.ui.fastDiceBox = FastDiceBox;
+
+            initializeToastr();
+        });
+
+    Hooks.once('ready', async () => {
+        await ui.fastDiceBox.render(true);
+    });
+
+
+    Hooks.on("renderSettingsConfig", (app, html, data) => {
+        let name, colour;
+        name = `fast-dice-box.diceColor`;
+        colour = game.settings.get("fast-dice-box", "diceColor");
+        $("<input>")
+            .attr("type", "color")
+            .attr("data-edit", name)
+            .val(colour)
+            .insertAfter($(`input[name="${name}"]`, html).addClass("color"));
+    });
+
+    Hooks.once("socketlib.ready", () => {
+        socket = socketlib.registerModule("fast-dice-box");
+        socket.register("newDiceRoll", newDiceRoll);
+    });
+};
+
+async function preloadTemplates() {
+    const templatePaths = [
+        'modules/fast-dice-box/templates/apps/fast-dice-box.html',
+    ];
+
+    return loadTemplates(templatePaths);
+}
+
