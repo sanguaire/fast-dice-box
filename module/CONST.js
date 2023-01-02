d0f39e596d7918b3dac2984e10f064c0ff99c72b
Andre Kurth <kurth.andre@scheidt-bachmann.de>
2023-01-02 07:47:31 +0100

    - adding default dice configuration

--- module/CONST.js
+++ module/CONST.js
@@ -1,3 +1,77 @@
 export const CONST = {
-    MODULE_NAME: "fast-dice-box"
+    MODULE_NAME: "fast-dice-box",
+    DEFAULT_DICE: {
+        1: {
+            active: true,
+            label: "@fdb.dice.d20",
+            formula: "d20",
+            img: "assets/d20-grey.svg"
+        },
+        2: {
+            active: true,
+            label: "@fdb.dice.d12",
+            formula: "d12",
+            img: "assets/d12-grey.svg"
+        },
+        3: {
+            active: true,
+            label: "@fdb.dice.d10",
+            formula: "d10",
+            img: "assets/d10-grey.svg"
+        },
+        4: {
+            active: true,
+            label: "@fdb.dice.d8",
+            formula: "d8",
+            img: "assets/d8-grey.svg"
+        },
+        5: {
+            active: true,
+            label: "@fdb.dice.d6",
+            formula: "d6",
+            img: "assets/d6-grey.svg"
+        },
+        6: {
+            active: true,
+            label: "@fdb.dice.d4",
+            formula: "d4",
+            img: "assets/d4-grey.svg"
+        },
+        7: {
+            active: true,
+            label: "@fdb.dice.two-d6",
+            formula: "2d6",
+            img: "assets/two-d6-grey.svg"
+        },
+        8: {
+            active: true,
+            label: "@fdb.dice.d100",
+            formula: "assets/d100-grey.svg",
+            img: ""
+        },
+        9: {
+            active: true,
+            label: "@fdb.dice.d66",
+            formula: "(d6*10)+d6",
+            img: "assets/two-d6-grey.svg"
+        },
+        10: {
+            active: false,
+            label: "",
+            formula: "",
+            img: ""
+        },
+        11: {
+            active: false,
+            label: "",
+            formula: "",
+            img: ""
+        },
+        12: {
+            active: false,
+            label: "",
+            formula: "",
+            img: ""
+        },
+    }
 }
