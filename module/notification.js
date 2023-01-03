8cb9e1cb1d02af7c4b72ab2b3cb109007b61dccb
André Kurth <mail@andre-kurth.de>
2023-01-03 14:38:48 +0100

    - changing some styling of the notification

--- module/notification.js
+++ module/notification.js
@@ -11,14 +11,16 @@ export const newDiceRoll = async (rollMessage) => {
 };
 
 export const initializeToastr = () => {
-    toastr.options.closeMethod = 'fadeOut';
+    toastr.options.closeMethod = "slideUp";
     toastr.options.closeDuration = 300;
-    toastr.options.timeOut = 5000;
-    toastr.options.extendedTimeOut = 10000;
+    toastr.options.timeOut = 8000;
+    toastr.options.extendedTimeOut = 13000;
     toastr.options.closeEasing = 'swing'
     toastr.options.newestOnTop = true;
     toastr.options.positionClass = "toast-top-right";
     toastr.options.escapeHtml = false;
     toastr.options.target = "#ui-middle";
+    toastr.options.showMethod = "slideDown";
+    toastr.options.hideMethod = "fadeOut"
 
 };
